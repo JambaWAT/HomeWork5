f66bd6e5e5809185019930c0e85298ad621049b7
JambaWAT <Puskinalex38@gmail.com>
2023-10-15 04:50:57 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        {
+            System.out.print("Задание1");
+            int clientOS = 1;
+            if (clientOS == 1)
+                System.out.print("Установите версию приложения для Android по ссылке1");
+            else if (clientOS == 0)
+                System.out.print("Установите версию приложения для iOS по ссылке");
+        }
+        {
+            System.out.print("Задание2");
+            int clientDeviceYear = 2013;
+            int clientOS2 = 0;
+            if (clientOS2 == 1 && clientDeviceYear < 2015)
+                System.out.print("Установите облегченную версию приложения для Android по ссылке");
+            else if (clientOS2 == 1 && clientDeviceYear >= 2015)
+                System.out.print("Установите версию приложения для Android по ссылке1");
+            if (clientOS2 == 0 && clientDeviceYear < 2015)
+                System.out.print("Установите облегченную версию приложения для ios по ссылке");
+            else if (clientOS2 == 0 && clientDeviceYear >= 2015)
+                System.out.print("Установите версию приложения для ios по ссылке1");
+        }
+        {
+            System.out.print("Задание3");
+            int year = 2020;
+            if (year < 1583) System.out.println(year + " год не являеится високосным");
+            else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                System.out.print(year + " год являеится високосным");
+        }
+        {
+            System.out.print("Задание4");
+            int deliveryDistance = 110;
+            if (deliveryDistance <= 20) System.out.println("Доставка займет 1 день");
+            else if (deliveryDistance > 20 && deliveryDistance <= 60) System.out.println("Доставка займет 2 дня");
+            else if (deliveryDistance > 60 && deliveryDistance <= 100) System.out.println("Доставка займет 3 дня");
+            else System.out.println("Доставка дальше 100 км не идет");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        }
+        {
+            System.out.print("Задание5 ");
+            int monthNumber = 9;
+            switch (monthNumber) {
+                case 1, 2, 12:
+                    System.out.println(monthNumber + " принадлежит к сезону Зима");
+                    break;
+                case 3, 5, 4:
+                    System.out.println(monthNumber + " принадлежит к сезону Весна");
+                    break;
+                case 6, 7, 8:
+                    System.out.println(monthNumber + " принадлежит к сезону Лето");
+                    break;
+                case 9, 10, 11:
+                    System.out.println(monthNumber + " принадлежит к сезону Осень");
+                    break;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+            }
         }
     }
 }
